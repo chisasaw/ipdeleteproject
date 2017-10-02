cd2050b65f7105bc77e3252939e80a5e78da1e59
chisasaw <chisasaw>
2017-10-02 12:41:48 -0500

    removed excess files

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-file=$1
-timestamp=`date +%s`
-
-#sort -r -k3 $file
-
-awk '!seen[$2]++' | awk '('$timestamp'-$3)/86400 > 365 {print $1 "    "$2"    " ('$timestamp' - $3)/86400" days    " $4}' $file | sort -nrk2
