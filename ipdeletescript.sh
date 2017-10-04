f7545a8c617b15e4b31a9b9ee9d7cc2b11a4c377
Pravir Adlakha <praviradlakha@hotmail.com>
2017-10-04 13:55:00 -0400

    Made sure seen awk function removes older ips first

--- ipdeletescript.sh
+++ ipdeletescript.sh
@@ -3,4 +3,4 @@
 file=$1
 timestamp=`date +%s`
 
-awk '!seen[$2]++' $file | awk '('$timestamp'-$3)/86400 > 365 {print $1 "    "$2"    " ('$timestamp' - $3)/86400" days    " $4}' | sort -nrk3 
+cat $file | sort -rk3 | awk '!seen[$2]++' | awk '('$timestamp'-$3)/86400 > 365 {print $1 "    "$2"    " ('$timestamp' - $3)/86400" days    " $4}' | sort -k2
